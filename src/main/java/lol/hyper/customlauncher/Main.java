3c7914b0af63b2d51b9abcc850617fd7d6f61023
hyperdefined <13475823+hyperdefined@users.noreply.github.com>
2021-05-27 14:16:05 -0400

    check if we can find the ttr path

--- src/main/java/lol/hyper/customlauncher/Main.java
+++ src/main/java/lol/hyper/customlauncher/Main.java
@@ -2,6 +2,7 @@ package lol.hyper.customlauncher;
 
 import lol.hyper.customlauncher.accounts.JSONManager;
 import lol.hyper.customlauncher.accounts.windows.MainWindow;
+import lol.hyper.customlauncher.updater.InvalidPath;
 import lol.hyper.customlauncher.updater.Updater;
 import org.json.JSONObject;
 
@@ -11,13 +12,23 @@ import java.nio.file.Paths;
 public class Main {
 
     public static String installPath;
+    public static final String DEFAULT_INSTALL = "C:\\Program Files (x86)\\Toontown Rewritten";
 
     public static void main(String[] args) {
+        // check to see if the default ttr install is there
+        // if not, use the path in the config
+        if (!Paths.get(DEFAULT_INSTALL).toFile().exists()) {
+            JSONObject optionsFile = JSONManager.readFile(JSONManager.configFile);
+            installPath = optionsFile.getString("ttrInstallLocation");
+            // check the config path
+            if (!Paths.get(installPath).toFile().exists()) {
+                InvalidPath invalidPath = new InvalidPath("Error");
+                System.exit(1);
+            }
+            JFrame updater = new Updater("Updater", Paths.get(installPath));
+        } else {
+            JFrame updater = new Updater("Updater", Paths.get(DEFAULT_INSTALL));
+        }
         JFrame mainWindow = new MainWindow("Launcher");
-
-        JSONObject optionsFile = JSONManager.readFile(JSONManager.configFile);
-        installPath = optionsFile.getString("ttrInstallLocation");
-
-        JFrame updater = new Updater("Updater", Paths.get(installPath));
     }
 }
---
+++ src/main/java/lol/hyper/customlauncher/updater/InvalidPath.java
@@ -0,0 +1,22 @@
+package lol.hyper.customlauncher.updater;
+
+import javax.swing.*;
+
+public class InvalidPath extends JFrame {
+
+    public InvalidPath(String title) {
+        JFrame frame = new JFrame(title);
+        frame.setSize(370, 150);
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        frame.setResizable(false);
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        JOptionPane.showMessageDialog(frame, "Unable to detect TTR install path. Please check the config.json and make sure that path is correct.", "Error", JOptionPane.ERROR_MESSAGE);
+        frame.setLocationRelativeTo(null);
+        frame.dispose();
+    }
+}
