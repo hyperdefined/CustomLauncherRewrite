7684a7f934fd3587be94d8ac10ac617cf9b2abe0
hyperdefined <contact@hyper.lol>
2021-07-09 14:53:53 -0400

    change user agent

--- src/main/java/lol/hyper/customlauncher/login/LoginHandler.java
+++ src/main/java/lol/hyper/customlauncher/login/LoginHandler.java
@@ -39,7 +39,7 @@ public class LoginHandler {
 
     private static final String REQUEST_URL = "https://www.toontownrewritten.com/api/login?format=json";
     private static final String USER_AGENT =
-            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36";
+            "CustomLauncherRewrite https://github.com/hyperdefined/CustomLauncherRewrite";
 
     /**
      * Handle the result of the login request. This will take a login request and act based on that
