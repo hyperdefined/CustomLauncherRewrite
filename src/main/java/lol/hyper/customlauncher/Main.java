7cfa9d6c3732be860dd70f93161e584538420891
hyperdefined <contact@hyper.lol>
2021-10-11 23:51:13 -0400

    version bump

--- src/main/java/lol/hyper/customlauncher/Main.java
+++ src/main/java/lol/hyper/customlauncher/Main.java
@@ -37,7 +37,7 @@ import java.nio.file.Paths;
 
 public class Main {
 
-    public static final String VERSION = "1.3.2";
+    public static final String VERSION = "1.3.3";
     public static Logger logger;
     public static String pathToUse;
 
