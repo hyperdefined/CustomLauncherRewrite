34917e404dc00342b8cc86d53edbeff39c9eb36a
hyperdefined <contact@hyper.lol>
2021-10-11 23:51:45 -0400

    Update MainWindow.java

--- src/main/java/lol/hyper/customlauncher/accounts/windows/MainWindow.java
+++ src/main/java/lol/hyper/customlauncher/accounts/windows/MainWindow.java
@@ -19,7 +19,6 @@ package lol.hyper.customlauncher.accounts.windows;
 
 import lol.hyper.customlauncher.accounts.Account;
 import lol.hyper.customlauncher.accounts.JSONManager;
-import lol.hyper.customlauncher.generic.InfoWindow;
 import lol.hyper.customlauncher.invasiontracker.InvasionTracker;
 import org.json.JSONObject;
 
@@ -95,7 +94,7 @@ public class MainWindow extends JFrame {
         // invasions button
         JButton invasionsButton = new JButton("Invasions");
         invasionsButton.addActionListener(e -> {
-                invasionTracker.showWindow();
+            invasionTracker.showWindow();
         });
         invasionsButton.setAlignmentX(Component.CENTER_ALIGNMENT);
         invasionsButton.setMaximumSize(new Dimension(300, invasionsButton.getMinimumSize().height));
