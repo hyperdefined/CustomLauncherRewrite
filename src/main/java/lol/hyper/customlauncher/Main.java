546eae0367ec487eed20f254a152a55eecc5aad5
hyperdefined <contact@hyper.lol>
2021-10-11 22:03:16 -0400

    Update Main.java

--- src/main/java/lol/hyper/customlauncher/Main.java
+++ src/main/java/lol/hyper/customlauncher/Main.java
@@ -37,7 +37,7 @@ import java.nio.file.Paths;
 
 public class Main {
 
-    public static final String VERSION = "1.3.1";
+    public static final String VERSION = "1.3.2";
     public static Logger logger;
     public static String pathToUse;
 
