17eab5bfada7f4e197a5c891eacc6392a679752b
hyperdefined <contact@hyper.lol>
2021-11-16 02:18:35 -0500

    more linux support stuff

--- linux/installer.sh
+++ linux/installer.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
-echo Dowloading latest version...
+
 # taken from https://gist.github.com/steinwaywhw/a4cd19cda655b8249d908261a62687f8
-curl -s hhttps://api.github.com/repos/hyperdefined/CustomLauncherRewrite/releases/latest \
-| grep "browser_download_url.*tar.gz" \
-| cut -d : -f 2,3 \
-| tr -d \" \
-| wget -qi -
+DOWNLOADURL=$(curl -s https://api.github.com/repos/hyperdefined/CustomLauncherRewrite/releases/latest | grep "browser_download_url"  | grep "tar.gz" | cut -d '"' -f 4)
+OUTPUTFILE=${DOWNLOADURL##*/}
+
+echo Dowloading latest version from $DOWNLOADURL
+
+wget -q -P /tmp/ https://api.upload.systems/images/download?id=bj022Ybq
+
+echo "We need to run -> 'sudo mkdir -p /opt/CustomLauncherRewrite'".
+sudo mkdir -p /opt/CustomLauncherRewrite
+
+echo Extracting $OUTPUTFILE...
+sudo tar -xf /tmp/$OUTPUTFILE -C /opt/CustomLauncherRewrite
+
+echo Dowloading desktop entry...
+sudo wget -q -P /usr/share/applications https://raw.githubusercontent.com/hyperdefined/CustomLauncherRewrite/master/linux/customlauncherrewrite.desktop
+
+echo Setting correct perms to install location...
+sudo chown -R $USER:$USER /opt/CustomLauncherRewrite
+
+echo CustomLauncherRewrite has been installed!
---
+++ linux/uninstaller.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo Removing /opt/CustomLauncherRewrite...
+
+sudo rm -r /opt/CustomLauncherRewrite
+
+echo Removing the desktop entry...
+sudo rm /usr/share/applications/customlauncherrewrite.desktop
+
+echo CustomLauncherRewrite has been uninstalled!
