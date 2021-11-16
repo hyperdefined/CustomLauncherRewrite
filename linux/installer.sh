98f62438db2968cb305cf8e7661910a10de80705
hyperdefined <contact@hyper.lol>
2021-11-16 02:51:23 -0500

    more installer fixes

--- linux/installer.sh
+++ linux/installer.sh
@@ -4,20 +4,22 @@
 DOWNLOADURL=$(curl -s https://api.github.com/repos/hyperdefined/CustomLauncherRewrite/releases/latest | grep "browser_download_url"  | grep "tar.gz" | cut -d '"' -f 4)
 OUTPUTFILE=${DOWNLOADURL##*/}
 
-echo Dowloading latest version from $DOWNLOADURL
+echo Dowloading latest version from "$DOWNLOADURL"
 
-wget -q -P /tmp/ https://api.upload.systems/images/download?id=bj022Ybq
+wget -q -P /tmp/ "$DOWNLOADURL"
 
 echo "We need to run -> 'sudo mkdir -p /opt/CustomLauncherRewrite'".
 sudo mkdir -p /opt/CustomLauncherRewrite
 
-echo Extracting $OUTPUTFILE...
-sudo tar -xf /tmp/$OUTPUTFILE -C /opt/CustomLauncherRewrite
+echo Extracting "$OUTPUTFILE"...
+sudo tar -xf /tmp/"$OUTPUTFILE" -C /opt/CustomLauncherRewrite
 
 echo Dowloading desktop entry...
 sudo wget -q -P /usr/share/applications https://raw.githubusercontent.com/hyperdefined/CustomLauncherRewrite/master/linux/customlauncherrewrite.desktop
 
 echo Setting correct perms to install location...
-sudo chown -R $USER:$USER /opt/CustomLauncherRewrite
+sudo chown -R "$USER":"$USER" /opt/CustomLauncherRewrite
+sudo chmod -R 755 /opt/CustomLauncherRewrite
+sudo rm -rf /tmp/CustomLauncherRewrite*
 
 echo CustomLauncherRewrite has been installed!
