e070ab55fd120363d27ca5705015d6950340fa53
hyperdefined <contact@hyper.lol>
2021-11-22 14:10:59 -0500

    Update installer.sh

--- linux/installer.sh
+++ linux/installer.sh
@@ -1,25 +1,26 @@
 #!/bin/bash
+[ "$(whoami)" != "root" ] && exec sudo -- "$0" "$@"
 
 # taken from https://gist.github.com/steinwaywhw/a4cd19cda655b8249d908261a62687f8
 DOWNLOADURL=$(curl -s https://api.github.com/repos/hyperdefined/CustomLauncherRewrite/releases/latest | grep "browser_download_url"  | grep "tar.gz" | cut -d '"' -f 4)
 OUTPUTFILE=${DOWNLOADURL##*/}
 
-echo Dowloading latest version from "$DOWNLOADURL"
+echo "Downloading latest version from ""$DOWNLOADURL"""
 
 wget -q -P /tmp/ "$DOWNLOADURL"
 
-echo "We need to run -> 'sudo mkdir -p /opt/CustomLauncherRewrite'".
-sudo mkdir -p /opt/CustomLauncherRewrite
+echo "Creating /opt/CustomLauncherRewrite...".
+mkdir -p /opt/CustomLauncherRewrite
 
-echo Extracting "$OUTPUTFILE"...
-sudo tar -xf /tmp/"$OUTPUTFILE" -C /opt/CustomLauncherRewrite
+echo "Extracting ""$OUTPUTFILE""..."
+tar -xf /tmp/"$OUTPUTFILE" -C /opt/CustomLauncherRewrite
 
-echo Dowloading desktop entry...
-sudo wget -q -P /usr/share/applications https://raw.githubusercontent.com/hyperdefined/CustomLauncherRewrite/master/linux/customlauncherrewrite.desktop
+echo "Downloading desktop entry..."
+wget -q -P /usr/share/applications https://raw.githubusercontent.com/hyperdefined/CustomLauncherRewrite/master/linux/customlauncherrewrite.desktop
 
-echo Setting correct perms to install location...
-sudo chown -R "$USER":"$USER" /opt/CustomLauncherRewrite
-sudo chmod -R 755 /opt/CustomLauncherRewrite
-sudo rm -rf /tmp/CustomLauncherRewrite*
+echo "Setting correct perms to install location..."
+chown -R "$USER":"$USER" /opt/CustomLauncherRewrite
+chmod -R 755 /opt/CustomLauncherRewrite
+rm -rf /tmp/CustomLauncherRewrite*
 
-echo CustomLauncherRewrite has been installed!
+echo "CustomLauncherRewrite has been installed!"
