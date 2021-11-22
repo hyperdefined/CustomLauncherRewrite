423190126df7237d8400bab7f2fe5792611d6afa
hyperdefined <contact@hyper.lol>
2021-11-22 13:25:57 -0500

    greatly improved extracting bzip2 files

--- src/main/java/lol/hyper/customlauncher/ttrupdater/TTRUpdater.java
+++ src/main/java/lol/hyper/customlauncher/ttrupdater/TTRUpdater.java
@@ -22,6 +22,7 @@ import lol.hyper.customlauncher.generic.ErrorWindow;
 import lol.hyper.customlauncher.generic.InfoWindow;
 import org.apache.commons.compress.compressors.bzip2.BZip2CompressorInputStream;
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.SystemUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -234,7 +235,7 @@ public class TTRUpdater extends JFrame {
             logger.info(filesToDownload.size() + " file(s) are going to be downloaded.");
             logger.info(filesToDownload);
 
-            progressBar.setValue(0); //reset
+            progressBar.setValue(0); // reset
 
             // download each file
             for (String fileToDownload : filesToDownload) {
@@ -269,7 +270,7 @@ public class TTRUpdater extends JFrame {
                 updateStatus.setText("Extracting file " + dl);
                 progressBar.setVisible(false);
                 try {
-                    extractFile(dl, fileToDownload); // extract the file to the new location
+                    decompressBz2(dl, fileToDownload); // extract the file to the new location
                 } catch (IOException e) {
                     logger.error("Unable to extract file" + dl, e);
                     JFrame errorWindow =
@@ -354,20 +355,19 @@ public class TTRUpdater extends JFrame {
     /**
      * Extract the compressed bzip2 files to their output file.
      *
-     * @param tempFile The temp file's name that was downloaded.
+     * @param temp The temp file's name that was downloaded.
      * @param outputName The file's output name.
      */
-    private static void extractFile(String tempFile, String outputName) throws IOException {
-        FileInputStream in = new FileInputStream("temp" + File.separator + tempFile);
-        FileOutputStream out =
-                new FileOutputStream(Main.TTR_INSTALL_DIR + File.separator + outputName);
-        BZip2CompressorInputStream bzIn = new BZip2CompressorInputStream(in);
-        byte[] buffer = new byte[4096];
-        int n;
-        while (-1 != (n = bzIn.read(buffer))) {
-            out.write(buffer, 0, n);
+    public void decompressBz2(String temp, String outputName) throws IOException {
+        File tempFile = new File("temp" + File.separator + temp);
+        File output = new File(Main.TTR_INSTALL_DIR + File.separator + outputName);
+        BZip2CompressorInputStream in =
+                new BZip2CompressorInputStream(
+                        new BufferedInputStream(new FileInputStream(tempFile)));
+        FileOutputStream out = new FileOutputStream(output);
+        try (in;
+                out) {
+            IOUtils.copy(in, out);
         }
-        out.close();
-        bzIn.close();
     }
 }
