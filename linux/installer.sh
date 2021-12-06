d36b0801b23fa4d6a554becce4a64f306a7ee3e3
Ah <ahmouse@protonmail.com>
2021-12-06 03:45:35 -0600

    Add desktop shortcut and icon

--- linux/installer.sh
+++ linux/installer.sh
@@ -2,23 +2,31 @@
 # taken from https://gist.github.com/steinwaywhw/a4cd19cda655b8249d908261a62687f8
 DOWNLOADURL=$(curl -s https://api.github.com/repos/hyperdefined/CustomLauncherRewrite/releases/latest | grep "browser_download_url"  | grep "tar.gz" | cut -d '"' -f 4)
 OUTPUTFILE=${DOWNLOADURL##*/}
+INSTALLDIR=/opt/CustomLauncherRewrite #In case support for a custom install directory is added
 
 echo "Downloading latest version from ""$DOWNLOADURL"""
 
 wget -q -P /tmp/ "$DOWNLOADURL"
 
-echo "Creating /opt/CustomLauncherRewrite...".
-sudo mkdir -p /opt/CustomLauncherRewrite
+echo "Creating $INSTALLDIR...".
+sudo mkdir -p $INSTALLDIR
 
 echo "Extracting ""$OUTPUTFILE""..."
-sudo tar -xf /tmp/"$OUTPUTFILE" -C /opt/CustomLauncherRewrite
+sudo tar -xf /tmp/"$OUTPUTFILE" -C $INSTALLDIR
 
 echo "Downloading desktop entry..."
+sudo wget -q -O ~/.local/share/icons/custom-rewrite-launcher.png https://github.com/hyperdefined/CustomLauncherRewrite/blob/master/src/main/resources/icon.png
 sudo wget -q -P /usr/share/applications https://raw.githubusercontent.com/hyperdefined/CustomLauncherRewrite/master/linux/customlauncherrewrite.desktop
 
+# Symlink on desktop (e.g. desktop shortcut)
+ln -s /usr/share/applications/customlauncherrewrite.desktop ~/Desktop/customlauncherrewrite.desktop 
+sudo chmod +x ~/Desktop/customlauncherrewrite.desktop
+sudo chown "$USER":"$USER" ~/Desktop/customlauncherrewrite.desktop
+
 echo "Setting correct perms to install location..."
-sudo chown -R "$USER":"$USER" /opt/CustomLauncherRewrite
-sudo chmod -R 755 /opt/CustomLauncherRewrite
+sudo chown -R "$USER":"$USER" $INSTALLDIR
+sudo chmod -R 755 $INSTALLDIR
+
 rm -rf /tmp/CustomLauncherRewrite*
 
 echo "CustomLauncherRewrite has been installed!"
--- linux/uninstaller.sh
+++ linux/uninstaller.sh
@@ -5,6 +5,7 @@ echo "Removing /opt/CustomLauncherRewrite..."
 sudo rm -r /opt/CustomLauncherRewrite
 
 echo "Removing the desktop entry..."
+sudo rm ~/Desktop/customlauncherrewrite.desktop
 sudo rm /usr/share/applications/customlauncherrewrite.desktop
 
 echo "CustomLauncherRewrite has been uninstalled!"
