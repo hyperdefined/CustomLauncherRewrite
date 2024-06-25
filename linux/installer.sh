933351806bb000e0776493c10de8619658493f16
hyperdefined <contact@hyper.lol>
2024-06-25 16:04:56 -0400

    better checking for user's desktop on linux install
    fixes #98

--- linux/installer.sh
+++ linux/installer.sh
@@ -32,20 +32,31 @@ echo "Extracting ""$OUTPUTFILE""..."
 sudo tar -xf /tmp/"$OUTPUTFILE" -C $INSTALLDIR
 
 echo "Downloading desktop entry..."
+# Create the icons directory just in case it doesn't exist
+# Save the icon for the launcher
 mkdir -p ~/.local/share/icons/
 wget -q -O ~/.local/share/icons/customlauncherrewrite-icon.png https://raw.githubusercontent.com/hyperdefined/CustomLauncherRewrite/master/src/main/resources/icon.png
-# create the icons directory just in case it doesn't exist
 sudo wget -q -P /usr/share/applications https://raw.githubusercontent.com/hyperdefined/CustomLauncherRewrite/master/linux/customlauncherrewrite.desktop
 
-# Symlink on desktop (e.g. desktop shortcut)
-ln -s /usr/share/applications/customlauncherrewrite.desktop ~/Desktop/customlauncherrewrite.desktop 
-sudo chmod +x ~/Desktop/customlauncherrewrite.desktop
-sudo chown "$USER":"$USER" ~/Desktop/customlauncherrewrite.desktop
+# Find the user's desktop
+if command -v xdg-user-dir >/dev/null 2>&1; then
+    DESKTOP_DIR=$(xdg-user-dir DESKTOP)
+else
+    # If xdg-user-dir is not installed, just try to guess it
+    DESKTOP_DIR=~/Desktop
+fi
 
+# Symlink on desktop (e.g. desktop shortcut)
+ln -s /usr/share/applications/customlauncherrewrite.desktop "$DESKTOP_DIR/customlauncherrewrite.desktop"
+sudo chmod +x "$DESKTOP_DIR/customlauncherrewrite.desktop"
+sudo chown "$USER":"$USER" "$DESKTOP_DIR/customlauncherrewrite.desktop"
+
+# Make sure the user owns the folder to run
 echo "Setting correct perms to install location..."
 sudo chown -R "$USER":"$USER" $INSTALLDIR
 sudo chmod -R 755 $INSTALLDIR
 
+# Delete any temp files
 rm -rf /tmp/CustomLauncherRewrite*
 
 echo "CustomLauncherRewrite has been installed!"
