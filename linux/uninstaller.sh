040df940424a519bdd516331d7da955d5b6fe38e
hyperdefined <contact@hyper.lol>
2024-06-26 19:15:31 -0400

    make the uninstaller better
    - fix #101
    - fix the prompt not appearing

--- linux/uninstaller.sh
+++ linux/uninstaller.sh
@@ -16,7 +16,10 @@
 # along with CustomLauncherRewrite.  If not, see <https://www.gnu.org/licenses/>.
 #
 
-read -r -p "Before we uninstall, would you like to keep your TTR files? These include screenshots and resource packs. Enter 'Y' to keep them, 'N' to wipe them.: " input
+read -r -p "Before we uninstall, would you like to keep your TTR files? These include screenshots and resource packs. Enter 'Y' to keep them, 'N' to wipe them.: " input < /dev/tty
+
+# Convert to uppercase
+input=$(echo "$input" | tr -d '[:space:]' | tr '[:lower:]' '[:upper:]')
 
 if [ "$input" != "Y" ]; then
   keep_ttr_files=false
@@ -30,13 +33,22 @@ if $keep_ttr_files; then
   sudo find /opt/CustomLauncherRewrite/* -type d -not -name 'ttr-files' -exec rm -rf {} +
   sudo rm /opt/CustomLauncherRewrite/CustomLauncherRewrite*
   sudo rm /opt/CustomLauncherRewrite/run.sh
-else 
+else
   echo "Removing /opt/CustomLauncherRewrite..."
   sudo rm -r /opt/CustomLauncherRewrite
 fi
 
+# Find the user's desktop
+if command -v xdg-user-dir >/dev/null 2>&1; then
+    DESKTOP_DIR=$(xdg-user-dir DESKTOP)
+else
+    # If xdg-user-dir is not installed, just try to guess it
+    DESKTOP_DIR=~/Desktop
+fi
+
 echo "Removing the desktop entry..."
-sudo rm ~/Desktop/customlauncherrewrite.desktop
+sudo rm "$DESKTOP_DIR/customlauncherrewrite.desktop"
+rm ~/.local/share/icons/customlauncherrewrite-icon.png
 sudo rm /usr/share/applications/customlauncherrewrite.desktop
 
 echo "CustomLauncherRewrite has been uninstalled!"
