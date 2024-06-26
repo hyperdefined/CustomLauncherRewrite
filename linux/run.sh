46fdbdb3f8db7020a63845becd454b7ebd0b6ad6
hyperdefined <contact@hyper.lol>
2024-06-26 19:09:25 -0400

    make sure the jar actually closes

--- linux/run.sh
+++ linux/run.sh
@@ -19,7 +19,19 @@
 # Check if Java is installed and the version is at least 17
 if (( $(java -version 2>&1 | grep -Po '(?<=")[0-9]{2}') >= 17 )); then
     latest=$(find . -name "CustomLauncherRewrite-*.jar" -type f -printf "%T@ %p\n" | sort -nr | head -n1 | cut -d' ' -f2-)
-    java -jar "$latest"
+    java -jar "$latest" &
+    pid=$!
+
+    # This is only needed as I've see the jar run in the background and not get closed
+    cleanup() {
+        if kill -0 $pid 2>/dev/null; then
+            echo "Killing CustomLauncherRewrite for cleanup, process $pid"
+            kill $pid
+        fi
+    }
+
+    trap cleanup EXIT
+    wait $pid
 else
     if command -v notify-send &> /dev/null; then
         notify-send -i stop "CustomLauncherRewrite" "Please update Java JRE to version 17 or higher!"
